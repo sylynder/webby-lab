5bb9fecbd0cf48d7e0fd01aaad5cb259fb7bb876
otengkwame <developerkwame@gmail.com>
2025-07-29 07:19:24 +0000

    :construction: wip
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- app/Views/website/partials/scripts.php
+++
@@ -1,5 +0,0 @@
-	<!-- JavaScript -->
-
-	<script src="<?=base_url()?>assets/website/js/jquery.bundle.js?ver=160"></script>
-	<script src="<?=base_url()?>assets/website/js/scripts.js?ver=160"></script>
-	<script src="<?=base_url()?>assets/website/js/charts.js"></script>
